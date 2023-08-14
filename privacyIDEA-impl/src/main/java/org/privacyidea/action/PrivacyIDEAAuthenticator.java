4e7819f9d3e5cce024fbdbb1d148bcbce29fab51
lukasmatusiewicz <77617779+lukasmatusiewicz@users.noreply.github.com>
2023-08-14 14:57:02 +0200

    Update PrivacyIDEAAuthenticator.java

--- privacyIDEA-impl/src/main/java/org/privacyidea/action/PrivacyIDEAAuthenticator.java
+++ privacyIDEA-impl/src/main/java/org/privacyidea/action/PrivacyIDEAAuthenticator.java
@@ -87,13 +87,13 @@ public class PrivacyIDEAAuthenticator extends AbstractChallengeResponseAction im
     }
 
     @Override
-    public void log(String message) {LOGGER.warn("PrivacyIDEA Client: " + message);} //todo change to info
+    public void log(String message) {LOGGER.info("PrivacyIDEA Client: " + message);}
 
     @Override
     public void error(String message) {LOGGER.error("PrivacyIDEA Client: " + message);}
 
     @Override
-    public void log(Throwable throwable) {LOGGER.warn("PrivacyIDEA Client: " + throwable);} //todo change to info
+    public void log(Throwable throwable) {LOGGER.info("PrivacyIDEA Client: " + throwable);}
 
     @Override
     public void error(Throwable throwable) {LOGGER.error("PrivacyIDEA Client: " + throwable);}
