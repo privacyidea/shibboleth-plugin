8557aef2a92deb86d22e73eee719e491834727c4
lukasmatusiewicz <77617779+lukasmatusiewicz@users.noreply.github.com>
2023-09-04 13:35:51 +0200

    cleanup

--- privacyIDEA-impl/src/main/java/org/privacyidea/action/PrivacyIDEAAuthenticator.java
+++ privacyIDEA-impl/src/main/java/org/privacyidea/action/PrivacyIDEAAuthenticator.java
@@ -2,22 +2,17 @@ package org.privacyidea.action;
 
 import javax.annotation.Nonnull;
 import javax.servlet.http.HttpServletRequest;
-import net.shibboleth.idp.authn.context.AuthenticationContext;
 import org.opensaml.profile.action.ActionSupport;
 import org.opensaml.profile.context.ProfileRequestContext;
-import org.privacyidea.IPILogger;
 import org.privacyidea.PIResponse;
-import org.privacyidea.PrivacyIDEA;
 import org.privacyidea.context.PIContext;
 import org.privacyidea.context.PIServerConfigContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class PrivacyIDEAAuthenticator extends AbstractChallengeResponseAction implements IPILogger
+public class PrivacyIDEAAuthenticator extends AbstractChallengeResponseAction
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(PrivacyIDEAAuthenticator.class);
-    private PrivacyIDEA privacyIDEA;
-    private boolean debug = false;
 
     /**
      * Constructor
@@ -25,49 +20,8 @@ public class PrivacyIDEAAuthenticator extends AbstractChallengeResponseAction im
     public PrivacyIDEAAuthenticator() {}
 
     @Override
-    protected final void doPreExecute(@Nonnull ProfileRequestContext profileRequestContext, @Nonnull PIContext piContext, @Nonnull PIServerConfigContext piServerConfigContext)
-    {
-        initPI(piServerConfigContext);
-
-        PIResponse triggerredResponse = null;
-        if (piServerConfigContext.getConfigParams().getTriggerChallenge())
-        {
-            if (debug)
-            {
-                LOGGER.info("{} Triggering the challenges...", this.getLogPrefix());
-            }
-            triggerredResponse = privacyIDEA.triggerChallenges(piContext.getUsername());
-        }
-
-        if (triggerredResponse != null)
-        {
-            if (triggerredResponse.error != null)
-            {
-                LOGGER.error("{} privacyIDEA server error: {}!", this.getLogPrefix(), triggerredResponse.error.message);
-                ActionSupport.buildEvent(profileRequestContext, "AuthenticationException");
-                return;
-            }
-
-            if (!triggerredResponse.multichallenge.isEmpty())
-            {
-                if (debug)
-                {
-                    LOGGER.info("{} Extracting the form data from triggered challenges...", this.getLogPrefix());
-                }
-                extractChallengeData(piContext, triggerredResponse);
-            }
-        }
-        else
-        {
-            LOGGER.error("{} triggerChallenge failed. Response was null. Fallback to standard procedure.", this.getLogPrefix());
-        }
-    }
-
-    @Override
     protected final void doExecute(@Nonnull ProfileRequestContext profileRequestContext, @Nonnull PIContext piContext, @Nonnull PIServerConfigContext piServerConfigContext)
     {
-        initPI(piServerConfigContext);
-
         HttpServletRequest request = this.getHttpServletRequest();
         if (request == null)
         {
@@ -97,8 +51,7 @@ public class PrivacyIDEAAuthenticator extends AbstractChallengeResponseAction im
 
                 if (piResponse != null)
                 {
-                    piContext.setMessage(piResponse.message);
-                    extractChallengeData(piContext, piResponse);
+                    extractChallengeData(piResponse);
 
                     if (piResponse.error != null)
                     {
@@ -133,79 +86,6 @@ public class PrivacyIDEAAuthenticator extends AbstractChallengeResponseAction im
                     }
                 }
             }
-        }
-    }
-
-    /**
-     * Prepare the privacyIDEA object.
-     * On/Off debug.
-     *
-     * @param piServerConfigContext Configuration for the privacyIDEA server.
-     */
-    private void initPI(@Nonnull PIServerConfigContext piServerConfigContext)
-    {
-        if (piServerConfigContext.getConfigParams().getDebug())
-        {
-            debug = piServerConfigContext.getConfigParams().getDebug();
-        }
-
-        if (privacyIDEA == null)
-        {
-            privacyIDEA = PrivacyIDEA.newBuilder(piServerConfigContext.getConfigParams().getServerURL(), "privacyIDEA-Shibboleth-Plugin")
-                                     .sslVerify(piServerConfigContext.getConfigParams().getVerifySSL())
-                                     .realm(piServerConfigContext.getConfigParams().getRealm())
-                                     .serviceAccount(piServerConfigContext.getConfigParams().getServiceName(), piServerConfigContext.getConfigParams().getServicePass())
-                                     .serviceRealm(piServerConfigContext.getConfigParams().getServiceRealm())
-                                     .logger(this)
-                                     .build();
-        }
-    }
-
-    /**
-     * Extract challenge data from server response, and save it in context.
-     *
-     * @param piContext privacyIDEA context
-     * @param piResponse server response
-     */
-    private void extractChallengeData(@Nonnull PIContext piContext, @Nonnull PIResponse piResponse)
-    {
-        if (piResponse.transactionID != null && !piResponse.transactionID.isEmpty())
-        {
-            piContext.setTransactionID(piResponse.transactionID);
-        }
-    }
-
-    // Log helper functions
-    @Override
-    public void log(String message)
-    {
-        if (debug)
-        {
-            LOGGER.info("PrivacyIDEA Client: " + message);
-        }
-    }
-    @Override
-    public void error(String message)
-    {
-        if (debug)
-        {
-            LOGGER.error("PrivacyIDEA Client: " + message);
-        }
-    }
-    @Override
-    public void log(Throwable throwable)
-    {
-        if (debug)
-        {
-            LOGGER.info("PrivacyIDEA Client: " + throwable);
-        }
-    }
-    @Override
-    public void error(Throwable throwable)
-    {
-        if (debug)
-        {
-            LOGGER.error("PrivacyIDEA Client: " + throwable);
         }
     }
 }
