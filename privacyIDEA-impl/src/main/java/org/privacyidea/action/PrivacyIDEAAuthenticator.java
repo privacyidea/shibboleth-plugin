66072a512526ceade9df437e4420753cb76cf2f2
lukasmatusiewicz <77617779+lukasmatusiewicz@users.noreply.github.com>
2023-10-05 08:55:41 +0200

    Update PrivacyIDEAAuthenticator.java

--- privacyIDEA-impl/src/main/java/org/privacyidea/action/PrivacyIDEAAuthenticator.java
+++ privacyIDEA-impl/src/main/java/org/privacyidea/action/PrivacyIDEAAuthenticator.java
@@ -1,6 +1,7 @@
 package org.privacyidea.action;
 
 import java.util.Map;
+import java.util.Objects;
 import javax.annotation.Nonnull;
 import javax.servlet.http.HttpServletRequest;
 import org.opensaml.profile.action.ActionSupport;
@@ -20,88 +21,78 @@ public class PrivacyIDEAAuthenticator extends AbstractChallengeResponseAction
     @Override
     protected final void doExecute(@Nonnull ProfileRequestContext profileRequestContext, @Nonnull PIContext piContext, @Nonnull PIServerConfigContext piServerConfigContext)
     {
-        HttpServletRequest request = this.getHttpServletRequest();
+        HttpServletRequest request = Objects.requireNonNull(this.getHttpServletRequestSupplier()).get();
         Map<String, String> headers = this.getHeadersToForward(request);
-        if (request == null)
+
+        piContext.setMode(request.getParameterValues("mode")[0]);
+        piContext.setWebauthnSignResponse(request.getParameterValues("webauthnSignResponse")[0]);
+        piContext.setOrigin(request.getParameterValues("origin")[0]);
+        piContext.setFormErrorMessage(request.getParameterValues("errorMessage")[0]);
+
+        PIResponse piResponse = null;
+        if (piContext.getWebauthnSignResponse() != null && !piContext.getWebauthnSignResponse().isEmpty())
         {
-            LOGGER.error("{} Profile action does not contain an HttpServletRequest.", this.getLogPrefix());
-            ActionSupport.buildEvent(profileRequestContext, "AuthenticationException");
+            if (piContext.getOrigin() == null || piContext.getOrigin().isEmpty())
+            {
+                LOGGER.error("Origin is missing for WebAuthn authentication!");
+            }
+            else
+            {
+                piResponse = privacyIDEA.validateCheckWebAuthn(piContext.getUsername(), piContext.getTransactionID(), piContext.getWebauthnSignResponse(),
+                                                               piContext.getOrigin(), headers);
+            }
         }
-        else
+        else if (piContext.getMode().equals("otp"))
         {
-            piContext.setMode(request.getParameterValues("mode")[0]);
-            piContext.setWebauthnSignResponse(request.getParameterValues("webauthnSignResponse")[0]);
-            piContext.setOrigin(request.getParameterValues("origin")[0]);
-            piContext.setFormErrorMessage(request.getParameterValues("errorMessage")[0]);
-            PIResponse piResponse = null;
-
-            if (piContext.getWebauthnSignResponse() != null && !piContext.getWebauthnSignResponse().isEmpty())
+            if (request.getParameterValues("pi_otp_input") != null)
             {
-                if (piContext.getOrigin() == null || piContext.getOrigin().isEmpty())
+                String otp = request.getParameterValues("pi_otp_input")[0];
+                if (otp != null)
                 {
-                    LOGGER.error("Origin is missing for WebAuthn authentication!");
-                }
-                else
-                {
-                    piResponse = privacyIDEA.validateCheckWebAuthn(piContext.getUsername(), piContext.getTransactionID(), piContext.getWebauthnSignResponse(),
-                                                                   piContext.getOrigin(), headers);
-                }
-            }
-            else if (piContext.getMode().equals("otp"))
-            {
-                if (request.getParameterValues("pi_otp_input") != null)
-                {
-                    String otp = request.getParameterValues("pi_otp_input")[0];
-                    if (otp != null)
-                    {
                     piResponse = privacyIDEA.validateCheck(piContext.getUsername(), otp, piContext.getTransactionID(), headers);
-                    }
-                    else
-                    {
-                        if (debug)
-                        {
-                            LOGGER.info("{} Cannot send password because it is null!", this.getLogPrefix());
-                        }
-                        return;
-                    }
-                }
-            }
-
-            if (piResponse != null)
-            {
-                extractChallengeData(piResponse);
-
-                if (piResponse.error != null)
-                {
-                    LOGGER.error("{} privacyIDEA server error: {}!", this.getLogPrefix(), piResponse.error.message);
-                    piContext.setFormErrorMessage(piResponse.error.message);
-                    ActionSupport.buildEvent(profileRequestContext, "reload");
-                }
-
-                if (!piResponse.multichallenge.isEmpty())
-                {
-                    if (debug)
-                    {
-                        LOGGER.info("{} Another challenge encountered. Building form...", this.getLogPrefix());
-                    }
-                    ActionSupport.buildEvent(profileRequestContext, "reload");
-                }
-                else if (piResponse.value)
-                {
-                    if (debug)
-                    {
-                        LOGGER.info("{} Authentication succeeded!", this.getLogPrefix());
-                    }
-                    ActionSupport.buildEvent(profileRequestContext, "success");
                 }
                 else
                 {
                     if (debug)
                     {
-                        LOGGER.info("{} Received a server message. Building form...", this.getLogPrefix());
+                        LOGGER.info("{} Cannot send password because it is null!", this.getLogPrefix());
                     }
-                    ActionSupport.buildEvent(profileRequestContext, "reload");
+                    return;
                 }
+            }
+        }
+        if (piResponse != null)
+        {
+            extractChallengeData(piResponse);
+            if (piResponse.error != null)
+            {
+                LOGGER.error("{} privacyIDEA server error: {}!", this.getLogPrefix(), piResponse.error.message);
+                piContext.setFormErrorMessage(piResponse.error.message);
+                ActionSupport.buildEvent(profileRequestContext, "reload");
+            }
+            if (!piResponse.multichallenge.isEmpty())
+            {
+                if (debug)
+                {
+                    LOGGER.info("{} Another challenge encountered. Building form...", this.getLogPrefix());
+                }
+                ActionSupport.buildEvent(profileRequestContext, "reload");
+            }
+            else if (piResponse.value)
+            {
+                if (debug)
+                {
+                    LOGGER.info("{} Authentication succeeded!", this.getLogPrefix());
+                }
+                ActionSupport.buildEvent(profileRequestContext, "success");
+            }
+            else
+            {
+                if (debug)
+                {
+                    LOGGER.info("{} Received a server message. Building form...", this.getLogPrefix());
+                }
+                ActionSupport.buildEvent(profileRequestContext, "reload");
             }
         }
     }
