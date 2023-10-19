8e234e1518c4a9cb3cb4ac1d3ecad5eddd5d98b0
lukasmatusiewicz <77617779+lukasmatusiewicz@users.noreply.github.com>
2023-10-19 10:51:59 +0200

    add image fields

--- privacyIDEA-impl/src/main/java/org/privacyidea/context/PIFormContext.java
+++ privacyIDEA-impl/src/main/java/org/privacyidea/context/PIFormContext.java
@@ -17,13 +17,19 @@ public class PIFormContext extends BaseContext
     @Nullable
     private final Integer otpLength;
     private String pollingInterval;
+    @Nullable
+    private String imageOtp;
+    @Nullable
+    private String imagePush;
+    @Nullable
+    private String imageWebauthn;
 
     public PIFormContext(@Nullable String defaultMessage, @Nullable String otpFieldHint, @Nullable Integer otpLength, @Nullable String pollingInterval)
     {
         this.defaultMessage = Objects.requireNonNullElse(defaultMessage, "Please enter the OTP!");
         this.otpFieldHint = Objects.requireNonNullElse(otpFieldHint, "OTP");
-        this.otpLength = otpLength;
         this.pollingInterval = Objects.requireNonNullElse(pollingInterval, "2");
+        this.otpLength = otpLength;
     }
 
     public void setMessage(String message) {this.message = message;}
@@ -43,4 +49,19 @@ public class PIFormContext extends BaseContext
     public Integer getOtpLength() {return otpLength;}
 
     public String getPollingInterval() {return pollingInterval;}
+
+    public void setImageOtp(@Nullable String imageOtp) {this.imageOtp = imageOtp;}
+
+    @Nullable
+    public String getImageOtp() {return imageOtp;}
+
+    public void setImagePush(@Nullable String imagePush) {this.imagePush = imagePush;}
+
+    @Nullable
+    public String getImagePush() {return imagePush;}
+
+    public void setImageWebauthn(@Nullable String imageWebauthn) {this.imageWebauthn = imageWebauthn;}
+
+    @Nullable
+    public String getImageWebauthn() {return imageWebauthn;}
 }
