0fcc4d396c1c34e03198ff758fae9a45c38d7c0c
lukasmatusiewicz <77617779+lukasmatusiewicz@users.noreply.github.com>
2024-01-18 12:35:54 +0100

    Create pi-main.js

---
+++ privacyIDEA-impl/src/main/resources/org/privacyidea/edit-webapp/js/pi-main.js
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2024 NetKnights GmbH - lukas.matusiewicz@netknights.it
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+function piMain()
+{
+    // ALTERNATE TOKEN SECTION VISIBILITY
+    if (piGetValue("webauthnSignRequest") === "" && !piGetValue("isPushAvailable"))
+    {
+        document.getElementById("alternateToken").style.display = "none";
+    }
+
+    // PUSH
+    if (piGetValue("mode") === "push")
+    {
+        document.getElementById("pi-form-submit-button").style.display = "none";
+        document.getElementById("pi_otp_input").style.display = "none";
+        window.onload = () =>
+        {
+            window.setTimeout(() =>
+            {
+                document.getElementById("pi-form-submit-button").click();
+            }, parseInt(piGetValue("pollingInterval")) * 1000);
+        }
+    }
+
+    // WEBAUTHN
+    if (document.getElementById("mode").value === "webauthn")
+    {
+        window.onload = () =>
+        {
+            doWebAuthn();
+        }
+    }
+    if (!window.location.origin)
+    {
+        window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
+    }
+    document.getElementById("origin").value = window.origin;
+}
+
+function doWebAuthn()
+{
+    // If we are in push mode, reload the page because in push mode the page refreshes every x seconds which could interrupt WebAuthn
+    // Afterward, WebAuthn is started directly
+    if (document.getElementById("mode").value === "push")
+    {
+        piChangeMode("webauthn");
+    }
+    else
+    {
+        try
+        {
+            const requestStr = document.getElementById("webauthnSignRequest").value;
+            const requestJSON = JSON.parse(requestStr);
+            const webAuthnSignResponse = window.pi_webauthn.sign(requestJSON);
+
+            webAuthnSignResponse.then((webauthnresponse) =>
+            {
+                document.getElementById("webauthnSignResponse").value = JSON.stringify(webauthnresponse);
+                document.getElementById("pi-form-submit-button").click();
+            });
+        }
+        catch (err)
+        {
+            console.log("Error while trying WebAuthn: " + err);
+            document.getElementById("errorMessage").value = "Error while trying WebAuthn: " + err;
+        }
+    }
+}
+
+// Wait until the document is ready
+document.addEventListener("DOMContentLoaded", function ()
+{
+    piMain();
+});
