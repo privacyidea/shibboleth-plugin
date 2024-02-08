d05c4627ce38641d7056fe7c4e9957da079e3c1a
lukasmatusiewicz <77617779+lukasmatusiewicz@users.noreply.github.com>
2024-02-08 13:44:25 +0100

    fix alternate token section visibility

--- privacyIDEA-impl/src/main/resources/org/privacyidea/edit-webapp/js/pi-main.js
+++ privacyIDEA-impl/src/main/resources/org/privacyidea/edit-webapp/js/pi-main.js
@@ -14,10 +14,40 @@
  * limitations under the License.
  */
 
+function doWebAuthn()
+{
+    // If we are in push mode, reload the page because in push mode the page refreshes every x seconds which could interrupt WebAuthn
+    // Afterward, WebAuthn is started directly
+    if (piGetValue("mode") === "push")
+    {
+        piChangeMode("webauthn");
+    }
+    else
+    {
+        try
+        {
+            const requestStr = piGetValue("webauthnSignRequest");
+            const requestJSON = JSON.parse(requestStr);
+            const webAuthnSignResponse = window.pi_webauthn.sign(requestJSON);
+
+            webAuthnSignResponse.then((webauthnresponse) =>
+            {
+                piSetValue("webauthnSignResponse", JSON.stringify(webauthnresponse));
+                piSubmit();
+            });
+        }
+        catch (err)
+        {
+            console.log("Error while trying WebAuthn: " + err);
+            piSetValue("errorMessage", "Error while trying WebAuthn: " + err);
+        }
+    }
+}
+
 function piMain()
 {
     // ALTERNATE TOKEN SECTION VISIBILITY
-    if (piGetValue("webauthnSignRequest") === "" && !piGetValue("isPushAvailable"))
+    if (piGetValue("webauthnSignRequest").length < 1 && piGetValue("isPushAvailable") !== true)
     {
         piDisableElement("alternateToken");
     }
@@ -47,36 +77,6 @@ function piMain()
         window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
     }
     piSetValue("origin", window.origin);
-}
-
-function doWebAuthn()
-{
-    // If we are in push mode, reload the page because in push mode the page refreshes every x seconds which could interrupt WebAuthn
-    // Afterward, WebAuthn is started directly
-    if (piGetValue("mode") === "push")
-    {
-        piChangeMode("webauthn");
-    }
-    else
-    {
-        try
-        {
-            const requestStr = piGetValue("webauthnSignRequest");
-            const requestJSON = JSON.parse(requestStr);
-            const webAuthnSignResponse = window.pi_webauthn.sign(requestJSON);
-
-            webAuthnSignResponse.then((webauthnresponse) =>
-            {
-                piSetValue("webauthnSignResponse", JSON.stringify(webauthnresponse));
-                piSubmit();
-            });
-        }
-        catch (err)
-        {
-            console.log("Error while trying WebAuthn: " + err);
-            piSetValue("errorMessage", "Error while trying WebAuthn: " + err);
-        }
-    }
 }
 
 // Wait until the document is ready
